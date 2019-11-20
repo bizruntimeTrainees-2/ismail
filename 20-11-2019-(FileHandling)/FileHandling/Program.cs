8731c40683b934519dde50b9840620620494dc7d
Ismail pathan <9553698730>
2019-11-20 20:29:45 +0530

    c#

---
+++ 20-11-2019-(FileHandling)/FileHandling/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceDir = @"E:\C-sharp\FileHandling\CurrentFile";
+            string backupDir = @"E:\C-sharp\FileHandling\BackupFile";
+            try
+            {
+                string[] picList = Directory.GetFiles(sourceDir,"*.PNG");
+                string[] textList = Directory.GetFiles(sourceDir, "*.txt");
+                //copying picture files
+                foreach(string f in picList)
+                {
+                    string fName = f.Substring(sourceDir.Length+1);
+                    // Use the Path.Combine method to safely append the file name to the path.
+                    // Will overwrite if the destination file already exists.
+                    File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fName), true);
+                }
+                foreach(string f in textList)
+                {
+                    string fName = f.Substring(sourceDir.Length + 1);
+                    try
+                    {
+                        // Will not overwrite if the destination file already exists.
+                        File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fName));
+                    }
+                    //catch exception if file is already copied
+                    catch(IOException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                //deleting source files that were copied
+                foreach(string f in textList)
+                {
+                    File.Delete(f);
+                }
+                foreach (string f in picList)
+                {
+                    File.Delete(f);
+                }
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ 20-11-2019-(FileHandling)/FileHandling_Append/FileHandling_Append/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace FileHandling_Append
+{
+    class Program
+    {
+        static string dataPath = @"E:\gitFolder\ismail\20-11-2019-(FileHandling)"; 
+        static void Main(string[] args)
+        {
+            CreatSimpleFile();
+
+            var aprilWeekends = from line in File.ReadLines(dataPath)
+                               where line.StartsWith("Saturday") ||
+                                     line.StartsWith("Sunday") &
+                                     line.Contains("April")
+                               select line;
+            File.WriteAllLines(@"E:\gitFolder\ismail\20-11-2019-(FileHandling)", aprilWeekends);
+
+            var mayMondays = from line in File.ReadLines(dataPath)
+                             where line.StartsWith("Monday") &
+                                   line.Contains("May")
+                             select line;
+            File.AppendAllLines(@"E:\gitFolder\ismail\20-11-2019-(FileHandling)", mayMondays);
+
+           Console.WriteLine("Done");
+        }
+        static void CreatSimpleFile()
+        {
+            DateTime timeStamp = new DateTime(2020,1,1);
+            try
+            {
+                using (StreamWriter sw = File.CreateText(dataPath))
+                {
+                    for (int i = 0; i < 500; i++)
+                    {
+                        DateTime ds1 = timeStamp.AddYears(i);
+                        DateTime ds2 = ds1.AddMonths(i);
+                        DateTime ds3 = ds2.AddDays(i);
+                        sw.WriteLine(ds3.ToLongDateString());
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("exception handled "+ ex.Message);
+            }
+                   }
+    }
+}
