15efa398fbbd867af927e38f4e0d446339101a24
Ismail pathan <9553698730>
2019-12-05 23:04:35 +0530

    c#

--- 12-11-2019-(collections,linq )/Dictionary.cs
+++ 12-11-2019-(collections,linq )/Dictionary.cs
@@ -1,54 +1,78 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Idictionary
+namespace CollectionsDc
 {
-    interface IValue
+    public class DictionaryD
     {
-        void Render();
-    }
-
-    class Content : IValue
-    {
-        public void Render()
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Render content");
-        }
-    }
-
-    class Image : IValue
-    {
-        public void Render()
-        {
-            Console.WriteLine("Render image");
-        }
-    }
-
-    class Dictionary_Demo
-    {
-        static void Main(string[] args)
-        {
-            var dictionary = new Dictionary<string, IValue>();
-            dictionary.Add("book", new Image());
-            dictionary.Add("bat", new Image());
-            dictionary.Add("pen", new Content());
-
-            // Look up interface objects and call implementations.
-            IValue value;
-
-            if (dictionary.TryGetValue("book", out value))
+            Dictionary<string, string> dis = new Dictionary<string, string>();
+            dis.Add("txt","value1");
+            dis.Add("doc","value2");
+            dis.Add("pdf","value3");
+            dis.Add("html","value4");
+            try
             {
-                value.Render(); 
+                dis.Add("txt","value5");
             }
-            if (dictionary.TryGetValue("bat", out value))
+            catch (ArgumentException)
             {
-                value.Render(); 
+                Console.WriteLine("adding key is already present");
             }
-            Console.ReadKey();
+           Console.WriteLine("for Key txt value: "+ dis["txt"]);
+           dis["txt"] = "value5";
+               Console.WriteLine("for Key txt changed value: "+ dis["txt"]);
+			Console.WriteLine();
+               try
+               {
+                Console.WriteLine("searching for key : "+ dis["dam"]);
+               }
+               catch (KeyNotFoundException)
+               {
+                           Console.WriteLine("searching key is not present");
+               }
+			Console.WriteLine();
+               string val="";
+               if (dis.TryGetValue("text", out val))
+               {
+                   Console.WriteLine("key value : "+ val);
+               }
+               else
+                   Console.WriteLine("key is not found");
+			Console.WriteLine();
+               if (!dis.ContainsKey("http"))
+               {
+                   dis.Add("http","Hello");
+                   Console.WriteLine("value for added key: "+ dis["http"]);
+               }
+               else 
+                    Console.WriteLine("key is already present");
+			Console.WriteLine();
+               foreach (KeyValuePair<string, string> kvp in dis)
+               {
+                   Console.WriteLine("key:"+ kvp.Key +"value: "+ kvp.Value);
+               }
+               Dictionary<string,string>.ValueCollection lval  = dis.Values;                                       //this is differ from sortedlist
+             Console.WriteLine();
+               foreach (string values in lval)
+               {
+                   Console.WriteLine("values: "+ values);
+               }
+               Console.WriteLine("retrieving through values: "+ dis.ContainsValue("Hello"));
+               Console.WriteLine();
+               Dictionary<string, string>.KeyCollection lkeys  = dis.Keys;                                                   // this is differ from sortedlist
+             Console.WriteLine();
+               foreach (string key in lkeys)
+               {
+                   Console.WriteLine("keys: "+ key);
+               }
+             Console.WriteLine("retrieving through values: "+ dis.ContainsKey("http"));
+			Console.WriteLine();
+               dis.Remove("txt");
+               if (!dis.ContainsKey("txt"))
+               {
+                          Console.WriteLine("key is not found");
+               }
         }
     }
-
 }
