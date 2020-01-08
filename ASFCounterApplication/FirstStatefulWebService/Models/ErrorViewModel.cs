b1ecf57bce1558caf009b6964401acca471846e9
unknown <ismailnani143@gmail.com>
2020-01-08 18:47:53 +0530

    service fabric simple examples

---
+++ ASFCounterApplication/FirstStatefulWebService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstStatefulWebService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ServicefabricApplication1/FirstStatelessAPIService/Controllers/FirstController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstStatelessAPIService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FirstController : Controller
+    {
+        [HttpGet]
+        [Route("get")]
+        public async  Task  Get()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
